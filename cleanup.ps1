8741cf5c9e2987f8262b396902f11fab13d65f5e
komiga2010 <44625746+komiga2010@users.noreply.github.com>
2018-10-31 11:58:57 +1100

    Create cleanup.ps1

---
+++ cleanup.ps1
@@ -0,0 +1,9 @@
+# script name: cleanup.ps1
+# script type: powershell script
+# author: komiga2010
+# version: 0.1
+# description: this script is used to cleanup unwanted files in folder, takes a list of file name/extensions as input, then it will 
+# scan through the nominated directory and sub-directories, output a list of files to be deleted for user review, once user reviews the
+# output, confirm the deletion.
+
+# 
